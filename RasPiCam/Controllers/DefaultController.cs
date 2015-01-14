b9551173c9758a42c78be7fd9621e0350b8dc6fc
James Gilmore <james.gilmore@red-gate.com>
2015-01-14 17:10:09 +0000

    Initial method for getting video list via API and JSON

--- RasPiCam/Controllers/DefaultController.cs
+++ RasPiCam/Controllers/DefaultController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using Microsoft.WindowsAzure;
 using RasPiCam.Models;
@@ -11,6 +12,32 @@ using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace RasPiCam.Controllers
 {
+    public class VideoApiController : ApiController
+    {
+        private const string c_blobContainer = "data";
+
+        [System.Web.Mvc.HttpGet]
+        [System.Web.Http.AcceptVerbs("GET")]
+        public System.Web.Http.Results.JsonResult<List<Video>> Videos()
+        {
+            var videosFound = new List<Video>();
+
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference(c_blobContainer);
+
+            foreach (IListBlobItem item in container.ListBlobs(null, false))
+            {
+                var blockBlob = item as CloudBlockBlob;
+                if (blockBlob == null) continue;
+                videosFound.Add(new Video(blockBlob.Name, blockBlob.Properties.Length));
+            }
+
+            return Json(videosFound);
+        }
+    }
+
     public class DefaultController : Controller
     {
         private const string c_blobContainer = "data";
