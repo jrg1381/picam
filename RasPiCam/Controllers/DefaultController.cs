3e63109b0766d628da92c211c7cea0c255cde585
James Gilmore <james.gilmore@red-gate.com>
2015-01-15 15:42:20 +0000

    Get the data for the table via AJAX.

--- RasPiCam/Controllers/DefaultController.cs
+++ RasPiCam/Controllers/DefaultController.cs
@@ -18,22 +18,7 @@ namespace RasPiCam.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            return View(VideosFromBlobStore().ToList());
-        }
-
-        private IEnumerable<Video> VideosFromBlobStore()
-        {
-            // Retrieve storage account from connection string.
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            CloudBlobContainer container = blobClient.GetContainerReference(c_blobContainer);
-
-            foreach (IListBlobItem item in container.ListBlobs(null, false))
-            {
-                var blockBlob = item as CloudBlockBlob;
-                if (blockBlob == null) continue;
-                yield return new Video(blockBlob.Name, blockBlob.Properties.Length, blockBlob.SnapshotQualifiedUri);
-            }
+            return View();
         }
     }
 }
