01c258b017931a546fd58061fb10c6cb8828f92e
James Gilmore <james.gilmore@red-gate.com>
2015-01-15 14:23:55 +0000

    Include jquery via a bundle.

---
+++ RasPiCam/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Optimization;
+
+namespace RasPiCam
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+        }
+    }
+}
