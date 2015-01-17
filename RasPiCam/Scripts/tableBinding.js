b841d7482dcc2b8b244a7174eef67c6ac22024b8
James Gilmore <james.gilmore@red-gate.com>
2015-01-17 22:58:20 +0000

    Now the controller name needs to be explicit again.

--- RasPiCam/Scripts/tableBinding.js
+++ RasPiCam/Scripts/tableBinding.js
@@ -9,7 +9,7 @@ function drawRow(rowData, tableId, timeStampFormat) {
     $(tableId).append(row);
     row.append($("<td>" + rowData.Size + "</td>"));
     row.append($("<td>" + moment(rowData.Timestamp).format(timeStampFormat) + "</td>"));
-    row.append($("<td>" + "<a class=\"btn btn-default\" href=\"/Video/" + rowData.EncodedFilename + "\">Download</a>" + "</td>"));
+    row.append($("<td>" + "<a class=\"btn btn-default\" href=\"/Default/Video/" + rowData.EncodedFilename + "\">Download</a>" + "</td>"));
 }
 
 function populateTable(data, tableId, timeStampFormat) {
