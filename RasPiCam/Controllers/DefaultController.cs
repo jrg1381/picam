8a5f17ba4390bded98db389b4034c1836ae82824
James Gilmore <james.gilmore@red-gate.com>
2015-01-23 15:33:47 +0000

    Inject the blob enumerator into the controller

--- RasPiCam/Controllers/DefaultController.cs
+++ RasPiCam/Controllers/DefaultController.cs
@@ -5,6 +5,18 @@ namespace RasPiCam.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IBlobEnumerator m_blobEnumerator;
+
+        public DefaultController()
+        {
+            m_blobEnumerator = BlobEnumeratorFactory.CreateInstance();
+        }
+
+        public DefaultController(IBlobEnumerator blobEnumerator)
+        {
+            m_blobEnumerator = blobEnumerator;
+        }
+
         // GET: Default
         public ActionResult Index()
         {
@@ -15,8 +27,7 @@ namespace RasPiCam.Controllers
         public ActionResult Video(string id)
         {
             var realName = Conversions.Base64ToUtf8String(id);
-            var blobEnumerator = BlobEnumeratorFactory.CreateInstance();
-            return Redirect(blobEnumerator.TemporaryUrlForBlob(realName).ToString());
+            return Redirect(m_blobEnumerator.TemporaryUrlForBlob(realName).ToString());
         }
     }
 }
