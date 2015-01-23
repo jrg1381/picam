e4c64eb95faa06da66ba81082f1d2b6008047b2f
James Gilmore <james.gilmore@red-gate.com>
2015-01-23 21:45:41 +0000

    Base64 strings are not valid ids
    Well they are sometimes, but not when they end in ==.

--- RasPiCam/Scripts/tableBinding.js
+++ RasPiCam/Scripts/tableBinding.js
@@ -1,10 +1,10 @@
 ﻿function drawTable(data, tableId, timeStampFormat) {
     for (var i = 0; i < data.length; i++) {
-        drawRow(data[i], tableId, timeStampFormat);
+        drawRow(data[i], tableId, timeStampFormat, i);
     }
 }
 
-function deleteVideo(id) {
+function deleteVideo(id, rowId) {
     $.ajax({
         url: "/api/VideoApi/Delete",
         data : { name : id },
@@ -13,19 +13,20 @@ function deleteVideo(id) {
             if (!data.Result) {
                 alert(data.ExceptionMessage);
             } else {
-                $("tr#" + id).remove(); // Better than querying the blob again
+                $("tr#" + rowId).remove(); // Better than querying the blob again
             }
         }
     });
 }
 
-function drawRow(rowData, tableId, timeStampFormat) {
-    var row = $("<tr id=\"" + rowData.EncodedFilename + "\"/>");
-    $(tableId).append(row);
+function drawRow(rowData, tableId, timeStampFormat, rowId) {
+    var id = tableId + "-" + rowId;
+    var row = $("<tr id=\"" + id + "\"/>");
+    $("#" + tableId).append(row);
     row.append($("<td width=\"10%\">" + rowData.Size + "</td>"));
     row.append($("<td width=\"60%\">" + moment(rowData.Timestamp).format(timeStampFormat) + "</td>"));
     row.append($("<td><a class=\"btn btn-default \" href=\"/Default/Video/" + rowData.EncodedFilename + "\">Download</a>&nbsp;" +
-        "<a class=\"btn btn-default \" href=\"javascript:deleteVideo('" + rowData.EncodedFilename + "')\">Delete</a>" + "</td>"));
+        "<a class=\"btn btn-default \" href=\"javascript:deleteVideo('" + rowData.EncodedFilename + "','" + id + "')\">Delete</a>" + "</td>"));
 }
 
 function populateTable(data, tableId, timeStampFormat) {
