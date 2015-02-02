a52fa0aa8a5ea4a6c9d9c241ee8439e6cd2795bf
James Gilmore <james.gilmore@red-gate.com>
2015-02-02 16:16:53 +0000

    Webjob for processing uploads (empty)

---
+++ ProcessVideos/Functions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace ProcessVideos
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new blob is uploaded
+        public static void ProcessQueueMessage([BlobTrigger("data/{name}")] Stream blob)
+        {
+            
+        }
+    }
+}
