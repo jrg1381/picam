fd7654eb07f1c355671108603442978ef5187ffb
James Gilmore <james.gilmore@red-gate.com>
2015-02-03 22:01:29 +0000

    This combination of flags should work
    Should be only committed blobs (None option) and the metadata.

--- RasPiCam/AzureBlob/BlobEnumerator.cs
+++ RasPiCam/AzureBlob/BlobEnumerator.cs
@@ -26,7 +26,7 @@ namespace RasPiCam.AzureBlob
             var blobClient = m_storageAccount.CreateCloudBlobClient();
             var container = blobClient.GetContainerReference(c_blobContainer);
 
-            foreach (IListBlobItem item in container.ListBlobs(null, false, BlobListingDetails.All))
+            foreach (IListBlobItem item in container.ListBlobs(null, false, BlobListingDetails.None | BlobListingDetails.Metadata))
             {
                 var blockBlob = item as CloudBlockBlob;
                 if (blockBlob == null) continue;
